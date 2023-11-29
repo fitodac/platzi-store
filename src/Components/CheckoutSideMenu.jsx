658243cd7db345b1f022d488daab431e606e9e87
fitodac <fitodac@gmail.com>
2023-11-28 23:54:03 -0300

    Rollup mini fix

--- src/Components/CheckoutSideMenu.jsx
+++ src/Components/CheckoutSideMenu.jsx
@@ -1,60 +1,56 @@
-import { useContext } from 'react'
-import { XMarkIcon } from '@heroicons/react/24/solid'
-import { ShoppingCartContext } from '../Context'
-import { OrderCard } from '.'
-import { totalPrice } from '../utils'
-import { Link } from 'react-router-dom'
-
+import { useContext } from 'react';
+import { XMarkIcon } from '@heroicons/react/24/solid';
+import { ShoppingCartContext } from '../Context';
+import { OrderCard } from '.';
+import { totalPrice } from '../Utils';
+import { Link } from 'react-router-dom';
 
 export const CheckoutSideMenu = () => {
-  
 	const {
 		cartProducts,
 		isCheckoutSideMenuOpen,
 		closeCheckoutSideMenu,
 		setCartProducts,
-		order, 
-		setOrder
-	} = useContext(ShoppingCartContext)
-
+		order,
+		setOrder,
+	} = useContext(ShoppingCartContext);
 
 	const handleDelete = (id) => {
-    const filteredProducts = cartProducts.filter(product => product.id != id)
-    setCartProducts(filteredProducts)
-  }
-
+		const filteredProducts = cartProducts.filter((product) => product.id != id);
+		setCartProducts(filteredProducts);
+	};
 
 	const handleCheckout = () => {
-    const orderToAdd = {
-      date: '01.02.23',
-      products: cartProducts,
-      totalProducts: cartProducts.length,
-      totalPrice: totalPrice(cartProducts)
-    }
+		const orderToAdd = {
+			date: '01.02.23',
+			products: cartProducts,
+			totalProducts: cartProducts.length,
+			totalPrice: totalPrice(cartProducts),
+		};
 
-    setOrder([...order, orderToAdd])
-    setCartProducts([])
-  }
-
+		setOrder([...order, orderToAdd]);
+		setCartProducts([]);
+	};
 
 	return (
 		<>
 			{isCheckoutSideMenuOpen && (
 				<aside
-					className="flex flex-col fixed right-0 border border-slate-300 rounded-md bg-white
-										w-[360px] h-[calc(100vh-68px)] top-[68px]">
-
+					className='flex flex-col fixed right-0 border border-slate-300 rounded-md bg-white
+										w-[360px] h-[calc(100vh-68px)] top-[68px]'
+				>
 					<div className='flex justify-between items-center p-6'>
 						<h2 className='font-medium text-xl'>My Order</h2>
 						<div>
 							<XMarkIcon
 								className='h-6 w-6 text-black cursor-pointer'
-								onClick={() => closeCheckoutSideMenu()}></XMarkIcon>
+								onClick={() => closeCheckoutSideMenu()}
+							></XMarkIcon>
 						</div>
 					</div>
 
-					<div className="px-6 overflow-y-scroll flex-1">
-						{cartProducts.map(product => (
+					<div className='px-6 overflow-y-scroll flex-1'>
+						{cartProducts.map((product) => (
 							<OrderCard
 								key={product.id}
 								{...product}
@@ -63,18 +59,24 @@ export const CheckoutSideMenu = () => {
 						))}
 					</div>
 
-					<div className="px-6 mb-6">
-						<p className="flex justify-between items-center mb-2">
+					<div className='px-6 mb-6'>
+						<p className='flex justify-between items-center mb-2'>
 							<span className='font-light'>Total:</span>
-							<span className='font-medium text-2xl'>${totalPrice(cartProducts)}</span>
+							<span className='font-medium text-2xl'>
+								${totalPrice(cartProducts)}
+							</span>
 						</p>
-						<Link to="/my-orders/last">
-							<button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}>Checkout</button>
+						<Link to='/my-orders/last'>
+							<button
+								className='bg-black py-3 text-white w-full rounded-lg'
+								onClick={() => handleCheckout()}
+							>
+								Checkout
+							</button>
 						</Link>
 					</div>
-
 				</aside>
 			)}
 		</>
-	)
-}
+	);
+};
